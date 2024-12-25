4a377d96d2b2f90fe6fb61857bdb05ae06591a48
Sourav199906 <166365696+Sourav199906@users.noreply.github.com>
2024-12-26 02:37:37 +0600

    Update login.js

--- login.js
+++ login.js
@@ -12,7 +12,7 @@ document.getElementById("login-form").addEventListener("submit", (e) => {
     if (user && user.email === email && user.password === password) {
         alert("Login successful!");
         localStorage.setItem("isAuthenticated", "true"); 
-        window.location.href = "home.html"; 
+        window.location.href = "index.html"; 
     } else {
         alert("Invalid credentials. Please try again.");
     }
